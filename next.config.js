e98925d9cab72cc69d02aed663378b42855e19ca
Tony Stark <afemiadom@icloud.com>
2023-02-28 21:47:15 -0600

    Added CI

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /**
+   * If you have the "experimental: { appDir: true }" setting enabled, then you
+   * must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
