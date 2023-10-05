31cb4d6ed661ad36f017b1a7600334a28d31ff28
Al Mithani <almithani@Als-MacBook-Pro-2.local>
2023-10-05 10:10:29 -0400

    updated blog image, link, partner images

--- components/Partner.tsx
+++ components/Partner.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Image from "next/image";
 import Link from "next/link";
-import opolis from "../public/images/partners/opolis.svg";
-import ityroot from "../public/images/partners/ityroot_dao.svg";
-import hdao from "../public/images/partners/hdao.svg";
 import daoPunk from "../public/images/partners/dao_punk.svg";
 import dao from "../public/images/partners/dao.svg";
+import nouns from "../public/images/partners/nouns.svg";
 import styles from "@/styles/partner.module.css"
 
 const Partner = () => {
@@ -16,36 +14,12 @@ const Partner = () => {
           Our Partners
         </h1>
         <div className='flex max-[767px]:flex-col justify-center items-center min-[767px]:gap-[9%] max-[767px]:gap-[4rem] p-[3%]'>
-            <div className="relative">
-                <Image
-                    priority
-                    src={opolis}
-                    className=""
-                    alt="bankless card"
-                />
-            </div>
-            <div className="relative z-50">
-                <Image
-                    priority
-                    src={ityroot}
-                    className=""
-                    alt="bankless card"
-                />
-            </div>
-            <div className="relative z-50">
-                <Image
-                    priority
-                    src={hdao}
-                    className=""
-                    alt="bankless card"
-                />
-            </div>
             <div className="relative z-50">
                 <Image
                     priority
                     src={daoPunk}
                     className=""
-                    alt="bankless card"
+                    alt="DAOpunks"
                 />
             </div>
             <div className="relative z-50">
@@ -53,7 +27,15 @@ const Partner = () => {
                     priority
                     src={dao}
                     className=""
-                    alt="bankless card"
+                    alt="BanklessDAO"
+                />
+            </div>
+            <div className="relative z-50">
+                <Image
+                    priority
+                    src={nouns}
+                    className=""
+                    alt="Nouns DAO"
                 />
             </div>
         </div>
