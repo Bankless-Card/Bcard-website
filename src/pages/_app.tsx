4f8adf11979530ee2c0819eb17c1e2f87cf6fdf0
Al Mithani <almithani@gmail.com>
2024-11-19 10:23:27 -0500

    adding google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  //return <Component {...pageProps} />
+  return (
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-WXX5WPLBE4`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-WXX5WPLBE4');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
